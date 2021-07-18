752cf7b71c126f3bc8b0f185e358e6fcf4eadf10
Dheeraj Komandur <36881069+dheeraj-komandur@users.noreply.github.com>
2021-07-18 23:45:27 +0530

    Header and Footer

---
+++ footer.php
@@ -0,0 +1,47 @@
+    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Footer -->
+<footer class="bg-dark text-white page-footer special-color-dark pt-4 ">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a href="https://www.facebook.com/sccnhub/" class="btn-floating btn-fb mx-1">
+          <i class="fa fa-facebook-f fa-lg"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a href="https://twitter.com/sccnhub" class="btn-floating btn-tw mx-1">
+          <i class="fa fa-twitter fa-lg"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a href="https://www.instagram.com/smart_campus/" class="btn-floating btn-gplus mx-1">
+          <i class="fa fa-instagram fa-lg"> </i>
+        </a>
+      </li>
+    </ul>
+    <!-- Social buttons -->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-1">© 2019 Copyright:
+    <a href="https://www.sccnhub.com/">Smart Campus Cloud Network</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+    </body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="icon" href="images/sccn_logo_inverted.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Link for font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- Google Chart -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
+    <title>SCCN</title>
+    
+    
+  </head>
+  
+  
+  <!--Navigation-->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    
+    <!-- Can Keep Image too <a href="#" class="navbar-brand"><img src="img/logo.png"></a>-->
+    
+    <a href="https://creationdevs.in/sccn" class="navbar-brand"><img style="width:85;height:50px;" src="images/sccn_logo.png"></a>
+
+    <!-- Button called when (navbar-expand-sm) small screen and populate it with id="navbarMenu"--> 
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    
+    <!-- Content of navbar is always ul -->
+    <div class="collapse navbar-collapse" id="navbarMenu">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="https://creationdevs.in/sccn/scope1.php" class="nav-link">Scope 1</a>
+            </li>
+            
+            <li class="nav-item">
+                <a href="https://creationdevs.in/sccn/scope2.php" class="nav-link">Scope 2</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://creationdevs.in/sccn/scope3.php" class="nav-link">Scope 3</a>
+            </li>
+            <li class="nav-item">
+                <a href="GHG_Manual.pdf" class="nav-link ">Download Manual</a>
+            </li>
+            <li class="nav-item">
+                <a href="https://www.sccnhub.com/contact-us" class="nav-link ">Contact Us</a>
+            </li>
+             
+        </ul>
+    </div>
+</nav>
+
+  
+  
+  <body>
+      
+  
+  
